0052f475175763ace218943aa8494d1c6a7d1e51
rhiaro <amy@rhiaro.co.uk>
2020-07-18 12:34:31 +0200

    post: make JSON-LD payload

--- src/post.php
+++ src/post.php
@@ -1,7 +1,17 @@
 <?php
 
 namespace rhiaro\ERH;
+use Requests;
+use ML\JsonLD\JsonLD;
 
+function make_jsonld_payload($graph){
+    $ns = ns();
+    $context = $ns->get("as");
+    $options = array("compactArrays" => true);
+    $jsonld = $graph->serialise("jsonld");
+    $compacted = JsonLD::compact($jsonld, $context, $options);
+    return JsonLD::toString($compacted, true);
+}
 
 function post_to_endpoint($endpoint, $key, $payload){
     $headers = array("Content-Type" => "application/ld+json", "Authorization" => $key);
